15be5950467212092430d8a2e025d29712ab693a
Ammon Taylor <ammonx9@gmail.com>
2026-02-15 18:29:32 -0700

    v0.9.6: Add waveform animation next to listening indicator

--- Sources/OpenWispr/main.swift
+++ Sources/OpenWispr/main.swift
@@ -5,7 +5,7 @@ setvbuf(stdout, nil, _IOLBF, 0)
 setvbuf(stderr, nil, _IOLBF, 0)
 
 enum OpenWispr {
-    static let version = "0.9.5"
+    static let version = "0.9.6"
 }
 
 let version = OpenWispr.version
