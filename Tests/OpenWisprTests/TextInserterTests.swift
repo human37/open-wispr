5695dddeb40594f06ebb4ee651fb51459a61eeac
Ammon Taylor <ammonx9@gmail.com>
2026-03-26 22:46:43 -0600

    Add AppleScript paste method as alternative to CGEvent

--- Tests/OpenWisprTests/TextInserterTests.swift
+++ Tests/OpenWisprTests/TextInserterTests.swift
@@ -6,4 +6,19 @@ final class TextInserterTests: XCTestCase {
     func testPasteKeyCodeIsHardcodedToVirtualV() {
         XCTAssertEqual(TextInserter.pasteKeyCode, 9, "Paste key code must be 9 (virtual 'V' position) to work across all keyboard layouts")
     }
+
+    func testDefaultPasteMethodIsCGEvent() {
+        let inserter = TextInserter(pasteMethod: nil)
+        XCTAssertEqual(inserter.resolvedPasteMethod, "cgevent")
+    }
+
+    func testAppleScriptPasteMethodIsRecognized() {
+        let inserter = TextInserter(pasteMethod: "applescript")
+        XCTAssertEqual(inserter.resolvedPasteMethod, "applescript")
+    }
+
+    func testUnknownPasteMethodDefaultsToCGEvent() {
+        let inserter = TextInserter(pasteMethod: "garbage")
+        XCTAssertEqual(inserter.resolvedPasteMethod, "cgevent")
+    }
 }
